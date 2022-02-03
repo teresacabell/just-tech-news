6ac7a8f8ff43c4a09b21da1000a805ea73292fed
teresacabell <teresacabll@gmail.com>
2022-02-03 13:11:45 -0600

    tested with insomnia

--- models/User.js
+++ models/User.js
@@ -34,18 +34,17 @@ User.init(
       }
     }
   },
-  { 
+  {
     hooks: {
       // set up beforeCreate lifecycle "hook" functionality
-      async beforeCreate(userData) {
-        newUserData.password = await bcrypt.hash(userData.password, 10);
-          return newUserData;
-        },
-        // set up beforeUpdate lifecycle "hook" functionality
-        async beforeUpdate(updatedUserData) {
-          updatedUserData.password = await bcrypt.hash(updatedUserData.password, 10);
-          return updatedUserData;
-        }
+      async beforeCreate(newUserData) {
+        newUserData.password = await bcrypt.hash(newUserData.password, 10);
+        return newUserData;
+      },
+      // set up beforeUpdate lifecycle "hook" functionality
+      async beforeUpdate(updatedUserData) {
+        updatedUserData.password = await bcrypt.hash(updatedUserData.password, 10);
+        return updatedUserData;
       }
     },
     sequelize,
