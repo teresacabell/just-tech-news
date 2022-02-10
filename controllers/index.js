2669aef06c3476e9c595f9a7d6098dfd125c0fde
teresacabell <teresacabll@gmail.com>
2022-02-10 16:38:09 -0600

    finished

--- controllers/index.js
+++ controllers/index.js
@@ -1,9 +1,13 @@
-const router = require('express').Router();
+const router = require("express").Router();
 
-const apiRoutes = require('./api/');
-const homeRoutes = require('./home-routes.js');
+const apiRoutes = require("./api/");
+const homeRoutes = require("./home-routes.js");
+const dashboardRoutes = require("./dashboard-routes");
 
-router.use('/', homeRoutes);
-router.use('/api', apiRoutes);
+router.use("/", homeRoutes);
+router.use("/api", apiRoutes);
+router.use("/", (req, res) => {
+  res.render("dashboard", { loggedIn: true });
+});
 
 module.exports = router;
